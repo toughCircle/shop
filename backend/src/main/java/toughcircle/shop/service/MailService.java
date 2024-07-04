2f89aebbe64ff95d8753586071b9c0762856809d
toughCircle <toughCircle@gmail.com>
2024-07-04 17:53:20 +0900

    feat: MailService

---
+++ backend/src/main/java/toughcircle/shop/service/MailService.java
@@ -0,0 +1,32 @@
+package toughcircle.shop.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendMail(String to, String subject, String htmlContent) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+        mailSender.send(message);
+    }
+}
