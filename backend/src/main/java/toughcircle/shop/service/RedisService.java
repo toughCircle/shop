6cff4cbfb6fe365459b1376fa25c8ac4dc2c5506
toughCircle <toughCircle@gmail.com>
2024-07-04 12:27:24 +0900

    feat: RedisConfig

---
+++ backend/src/main/java/toughcircle/shop/service/RedisService.java
@@ -0,0 +1,27 @@
+package toughcircle.shop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String key, Object value, long timeout, TimeUnit unit) {
+        redisTemplate.opsForValue().set(key, value, timeout, unit);
+    }
+
+    public Object find(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+}
