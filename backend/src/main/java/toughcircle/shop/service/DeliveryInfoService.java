1c7673893a81b3375ae6c040a96a30fb5bcc15f1
toughCircle <toughCircle@gmail.com>
2024-08-08 21:13:44 +0900

    fix: Remove import

--- backend/src/main/java/toughcircle/shop/service/DeliveryInfoService.java
+++ backend/src/main/java/toughcircle/shop/service/DeliveryInfoService.java
@@ -6,13 +6,10 @@ import toughcircle.shop.model.Entity.Address;
 import toughcircle.shop.model.Entity.DeliveryInfo;
 import toughcircle.shop.model.Entity.Order;
 import toughcircle.shop.model.dto.DeliveryInfoDto;
-import toughcircle.shop.repository.DeliveryInfoRepository;
 
 @Service
 @RequiredArgsConstructor
 public class DeliveryInfoService {
-
-    private final DeliveryInfoRepository deliveryInfoRepository;
 
     public DeliveryInfo createDeliveryInfo(DeliveryInfoDto infoDto, Order order) {
         Address address = new Address();
