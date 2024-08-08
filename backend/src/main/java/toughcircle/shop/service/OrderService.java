7797f067705a0fa187d06aa216d425e45f3d639a
toughCircle <toughCircle@gmail.com>
2024-08-09 02:58:34 +0900

    fix: Update Service

--- backend/src/main/java/toughcircle/shop/service/OrderService.java
+++ backend/src/main/java/toughcircle/shop/service/OrderService.java
@@ -3,7 +3,6 @@ package toughcircle.shop.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import toughcircle.shop.model.Entity.*;
 import toughcircle.shop.model.Enums.DeliveryStatus;
